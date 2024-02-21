5291b2ca0c86db49a7c1ba39c84da350fc3686bb
Patrick Njiru <patricknjiru93@gmail.com>
2024-02-21 23:35:50 +0300

    Add images, install packages and configure files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  screens: {
+    sm: '600px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200',
+  },
   theme: {
     extend: {},
   },
